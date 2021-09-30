85bf5b3061f27ea5881bfdb0f57a75e99cf7280c
URITI VENKATA NAGA KISHORE <kishoreuriti@gmail.com>
2021-09-30 23:32:00 +0530

    Update backend.tf

--- AppServiceSQLDB/backend.tf
+++ AppServiceSQLDB/backend.tf
@@ -1,10 +1,12 @@
 # We strongly recommend using the required_providers block to set the
 # Azure Provider source and version being used
 terraform {
+  required_version = ">=0.11"
   backend "azurerm" {
     resource_group_name  = "__terraformstoragerg__"
     storage_account_name = "__terraformstorageaccount__"
     container_name       = "__terraformstoragecontainer__"
     key                  = "__terraformstoragekey__"
+    access_key           = "__storagekey__"
   }
 }
