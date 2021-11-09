71ee38c2416ed7224099bcb0bea2a35e4a675a46
URITI VENKATA NAGA KISHORE <kishoreuriti@gmail.com>
2021-11-09 23:08:24 +0530

    Update main.tf

--- Covid19-ADF/main.tf
+++ Covid19-ADF/main.tf
@@ -1,1 +1,10 @@
+# Creating a resource group pulls values from tfvars 
 
+##################################
+#       Azure Resource Group     #
+##################################
+
+resource "azurerm_resource_group" "base_rg" {
+  name     = "${var.resource_group_name}-${var.environment}-rg"
+  location = var.location
+}
